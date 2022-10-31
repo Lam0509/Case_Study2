255d772fe473678853694e1031fedb1060567b1b
Lam0509 <112493001+Lam0509@users.noreply.github.com>
2022-10-31 11:08:54 +0700

    third commit

--- Src/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var DictionaryCotroller_1 = require("./Controller/DictionaryCotroller");
-DictionaryCotroller_1.DictionaryController.menuDictionaryController();
